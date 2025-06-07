556878a554b61da7a077a11875334d5453d4bf99
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-06 21:44:39 -0400

    feat: nuxt ui pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/ui-pro'],
   devtools: { enabled: true },
   compatibilityDate: '2025-05-15',
   eslint: { config: { stylistic: true } },
+  css: ['~/assets/css/main.css'],
 })
