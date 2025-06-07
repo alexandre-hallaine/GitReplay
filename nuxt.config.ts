72d0c5924417e7b9b48016ab5dc1ba540977f06b
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-07 00:01:29 -0400

    chore: set up base Nuxt modules, ESLint integration, and project scaffolding (#1)
    * fix(package): remove vue and update pnpm
    * fix: Readme
    * feat: eslint
    * feat: nuxt ui pro
    * feat: nuxt hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/ui-pro', '@nuxthub/core'],
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  hub: { workers: true },
+  eslint: { config: { stylistic: true } },
 })
