50e80f2925e98b3d18491b7bc01897aca74d4908
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-08 14:45:40 -0400

    feat(head): title and lang

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/ui-pro', '@nuxthub/core'],
   $development: { hub: { remote: 'production' } },
   devtools: { enabled: true },
+  app: { head: {
+    title: 'GitReplay',
+    htmlAttrs: {
+      lang: 'en',
+    },
+  } },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-05-15',
   nitro: { experimental: { openAPI: true } },
