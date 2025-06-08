c2c9bc14251df1cac362e3b0c942ebb945e1e1ac
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-08 16:23:12 -0400

    feat: github only 2025 (test)

--- server/api/profile.ts
+++ server/api/profile.ts
@@ -3,20 +3,8 @@ export default defineEventHandler(async (event) => {
   const login = getQuery(event).login as string
   if (!login) throw createError({ statusCode: 400, message: '"login" parameter is missing.' })
 
-  const years = await fetchContributionYears(octokit, login) // Pass octokit
-  console.log(years)
-
-  const allDays = (
-    await Promise.all(
-      years.map(async (year) => {
-        const collection = await fetchContributionDataForYear(octokit, login, year)
-        console.log(year, 'done')
-        return collection.contributionCalendar.weeks.flatMap(w => w.contributionDays)
-      }),
-    )
-  ).flat()
-
-  const { currentStreak, longestStreak } = computeStreaks(allDays)
+  const collection = await fetchContributionDataForYear(octokit, login, 2025)
+  const { currentStreak, longestStreak } = computeStreaks(collection.contributionCalendar.weeks.flatMap(w => w.contributionDays))
 
   return {
     currentStreak,
