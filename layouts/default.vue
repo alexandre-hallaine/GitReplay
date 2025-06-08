943c01b9f03e8a18264881bc21a037dc42afd2bc
Alexandre Hallaine <alexandre@hallaine.com>
2025-06-08 14:40:22 -0400

    feat(layout): introduce header layout and user page (#5)
    * fix(remote): force production
    
    * feat: layout
    
    * fix(header): move title in the header

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Me',
+    to: '/me',
+  },
+])
+</script>
+
+<template>
+  <div>
+    <UHeader title="GitReplay">
+      <UNavigationMenu :items="items" />
+
+      <template #right>
+        <UColorModeButton />
+
+        <UButton
+          color="neutral"
+          variant="ghost"
+          to="https://github.com/alexandre-hallaine/GitReplay"
+          target="_blank"
+          icon="lucide:github"
+        />
+      </template>
+
+      <template #body>
+        <UNavigationMenu
+          :items="items"
+          orientation="vertical"
+        />
+      </template>
+    </UHeader>
+    <slot />
+  </div>
+</template>
