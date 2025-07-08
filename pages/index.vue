a5efa4ad71e892f06770867ea831d80aa81a8636
Alexandre Hallaine <alexandre@hallaine.com>
2025-07-08 14:27:18 -0400

    feat(chart): versatile

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,10 @@
 <script setup lang="ts">
-interface Stats {
-  events: { [key: string]: number }
-  hours: { [key: string]: number }
-  languages: { [key: string]: number }
+const { data: rawStats } = await useFetch<Record<string, Stats>>('/api/stats')
+
+const stats = {
+  languages: normalize(filter(extract(rawStats.value!, 'languages'), 15)),
+  events: extract(rawStats.value!, 'events'),
 }
-
-const { data: stats } = await useFetch<{ [key: string]: Stats }>('/api/stats')
-
-const data = Object.fromEntries(
-  Object.entries(stats.value || {})
-    .map(([key, value]) => [
-      new Date(key).getTime(),
-      value.languages,
-    ])
-    .filter(([_, value]) => value),
-)
-
-const keys = computed(() => {
-  if (stats.value == null) return []
-
-  const dates = Object.keys(stats.value)
-  const date = dates.sort()[dates.length - 1]
-  const { languages } = stats.value[date]
-
-  return Object.entries(languages)
-    .sort((a, b) => b[1] - a[1])
-    .slice(0, 6).map(([lang]) => lang)
-})
 </script>
 
 <template>
@@ -34,11 +12,11 @@ const keys = computed(() => {
     title="Start exploring"
     description="Explore deep GitHub insights — no login required."
   >
-    <UCard>
-      <StackedArea
-        :keys
-        :data
-      />
-    </UCard>
+    <UPageCard title="Top Languages Over Time">
+      <MultiLine :data="Object.entries(stats.languages).map(([k, v]) => [+k, v])" />
+    </UPageCard>
+    <UPageCard title="Events Breakdown">
+      <MultiLine :data="Object.entries(stats.events).map(([k, v]) => [+k, v])" />
+    </UPageCard>
   </UPageSection>
 </template>
