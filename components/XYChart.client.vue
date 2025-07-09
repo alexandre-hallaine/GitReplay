7c90c3a8095f5f6d9979ea308f0abacb213cf75c
Alexandre Hallaine <alexandre@hallaine.com>
2025-07-09 00:32:46 -0400

    fix(XYChart): axis

--- components/XYChart.client.vue
+++ components/XYChart.client.vue
@@ -1,21 +1,31 @@
 <script setup lang="ts">
 import { VisXYContainer, VisLine, VisArea, VisBulletLegend, VisAxis } from '@unovis/vue'
 
-const { data, showArea } = defineProps<{
+const { data } = defineProps<{
   data: [number, Record<string, number>][]
   showArea?: boolean
 }>()
 
-const keys = computed(() => data.length ? Object.keys(data[0][1]) : [])
-const x = (d: [number, Record<string, number>]) => d[0]
-const y = keys.value.map(k => (d: [number, Record<string, number>]) => d[1][k])
+const keys = computed(() => (data[0] ? Object.keys(data[0][1]) : []))
+
+// x returns fractional year
+const x = (d: [number, Record<string, number>]) => {
+  const date = new Date(d[0])
+  const year = date.getFullYear()
+  const start = new Date(year, 0, 1).getTime()
+  const end = new Date(year + 1, 0, 1).getTime()
+  return year + (d[0] - start) / (end - start)
+}
+
+// y is an array of accessors for each key
+const y = computed(() => keys.value.map(k => (d: [number, Record<string, number>]) => d[1][k]))
 </script>
 
 <template>
   <div>
     <VisBulletLegend
       v-if="keys.length > 1"
-      :items="keys.map((key) => ({ name: key }))"
+      :items="keys.map(name => ({ name }))"
     />
     <VisXYContainer :data>
       <component
@@ -23,10 +33,7 @@ const y = keys.value.map(k => (d: [number, Record<string, number>]) => d[1][k])
         :x
         :y
       />
-      <VisAxis
-        type="x"
-        :tick-format="(value: number) => Intl.DateTimeFormat().format(value)"
-      />
+      <VisAxis type="x" />
     </VisXYContainer>
   </div>
 </template>
